77d074a2700f89be9797b7214716e7176bcf6dbe
Krypto_Nato05 <naoto.ushizaki@gmail.com>
2025-09-30 21:51:30 -0300

    Update Main_Control.java

--- src/Main_Control.java
+++ src/Main_Control.java
@@ -21,13 +21,13 @@ public class Main_Control {
             Drone drone1 = new Drone("DRN001", "DJI Phantom", StatusDrone.PRONTO, 25.0f);
 
             // Criando uma missão
-            Missao missao1 = new Missao("MS001", "Área 12 - Soja", "2025-10-01");
+            Mission missao1 = new Mission("MS001", "Área 12 - Soja", "2025-10-01");
 
             // Enviando parâmetros da missão
-            drone1.receberParametrosMissao(missao1);
+            drone1.receberParametrosMission(missao1);
 
             // Executando a missão
-            drone1.executarMissao();
+            drone1.executarMission();
 
             // Verificando evento crítico
             boolean evento = drone1.detectarEventoCritico();
@@ -47,3 +47,4 @@ public class Main_Control {
         scanner.close();
     }
 }
+
