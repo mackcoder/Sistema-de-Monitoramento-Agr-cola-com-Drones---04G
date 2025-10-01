4133104d1ded68c6d16d8036c5e3710fc6e7c50b
Krypto_Nato05 <naoto.ushizaki@gmail.com>
2025-09-30 22:38:39 -0300

    Update Main_Control.java

--- src/Main_Control.java
+++ src/Main_Control.java
@@ -14,11 +14,16 @@ public class Main_Control {
         if (valida.validar(usuario, login, password)) {
             System.out.println("✅ Login autorizado. Bem-vindo, " + usuario.getNome());
             
-            Drone drone1 = new Drone("DRN001", "DJI Phantom", StatusDrone.PRONTO, 25.0f);
+            Drone drone1 = new Drone("RDBLL5", "Red Bull", StatusDrone.PRONTO, 25.0f);
 
-            Missao missao = new Missao("MS003", "Monitoramento da Área 5 - Milho",
+            // Chamada do Banco de Dados
+            bd_drone bd = new bd_drone(); 
+            bd.inserirDrone(drone1);
+            bd.listarDrones();
+
+            Missao missao = new Missao("MS003", "Monitoramento da Área 5 - Soja",
                     Arrays.asList("Captura de imagem", "Leitura de temperatura"));
-
+            // Recebe info da missao distribuida:
             drone1.receberParametrosMissao(missao);
             drone1.executarMissao(missao);
 
@@ -35,4 +40,5 @@ public class Main_Control {
         scanner.close();
     }
 }
+
 
