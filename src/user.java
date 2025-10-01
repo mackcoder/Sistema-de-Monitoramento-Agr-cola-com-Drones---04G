5122a26d77c64c73ac76f3ed023841ca6c365d71
Krypto_Nato05 <naoto.ushizaki@gmail.com>
2025-09-30 22:17:57 -0300

    Update user.java

--- src/user.java
+++ src/user.java
@@ -1,1 +1,26 @@
+public class User {
+    private String nome;
+    private String login;
+    private String password;
+
+    public User(String nome, String login, String password) {
+        this.nome = nome;
+        this.login = login;
+        this.password = password;
+    }
+
+    // Método de autenticação simples
+    public boolean autenticar(String loginInformado, String senhaInformada) {
+        return this.login.equals(loginInformado) && this.senha.equals(senhaInformada);
+    }
+
+    public String getNome() { return nome; }
+    public String getLogin() { return login; }
+    public String getpassword() { return password; }
+
+    public void setNome(String nome) { this.nome = nome; }
+    public void setLogin(String login) { this.login = login; }
+    public void setpassword(String password) { this.senha = password; }
+}
+
 
